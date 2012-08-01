9883ff775c1592d040a25a1f4198021fc5121880
Michael Helmling <helmling@mathematik.uni-kl.de>
2012-08-01 15:16:49 +0200

    fix setup.py

--- lpd_research/setup.py
+++ lpd_research/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import numpy
-default_includes = [numpy.get_include(), ".", "src"]
+default_includes = [numpy.get_include(), ".", "lpresearch"]
 ext_modules = [Extension("lpresearch.cspdecoder", ["lpresearch/cspdecoder.pyx"], include_dirs=default_includes)]
 
 setup(
