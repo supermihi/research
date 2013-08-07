31eee48e631e519f4646df03bbbc149a21acf95a
Michael Helmling <michaelhelmling@posteo.de>
2013-08-07 15:35:17 +0200

    Added wolfe's ad-hoc degeneracy procedure to DantzigWolfe

--- lpd_research/lpresearch/dantzigwolfe.py
+++ lpd_research/lpresearch/dantzigwolfe.py
@@ -56,7 +56,6 @@ class DantzigWolfeTurboDecoder(Decoder):
         (t1, s1, b1), (t2, s2, b2) = self.pairs[j]
         val1 = self.llrVector[self.segAndLabForCodeBit[(t1[s1],b1)]]
         val2 = self.llrVector[self.segAndLabForCodeBit[(t2[s2],b2)]]
-        assert val1 == val2
         if val == 0:
             if val1 + val2 > 0:
                 bit1 = bit2 = 0
@@ -71,13 +70,11 @@ class DantzigWolfeTurboDecoder(Decoder):
         setattr(t2[s2], "fix_{}".format(labelStr[b2]), bit2)
         
     def solve(self, hint=None, lb=1):
-        # find starting basis
         self.code.setCost(self.llrVector)
         B = np.empty( (self.m, self.m) )
         c = np.zeros(self.m)
         B[0,:] = 1
         codewords = [np.zeros(self.code.blocklength) for _ in range(self.m)]
-        mu = np.zeros(self.k)
         for j in range(self.k):
             self.fixConstraintValue(j, 0)
         for j in range(self.m):
@@ -85,7 +82,7 @@ class DantzigWolfeTurboDecoder(Decoder):
                 self.fixConstraintValue(j-1, 1)
             g_result = np.zeros(self.k)
             for enc in self.code.encoders:
-                c[j] += shortestPathScalarization(enc.trellis, 1, mu, g_result, codewords[j])
+                c[j] += shortestPathScalarization(enc.trellis, 1, np.zeros(self.k), g_result, codewords[j])
             B[1:, j] = g_result
             if j > 0:
                 self.fixConstraintValue(j-1, -1)
@@ -94,42 +91,61 @@ class DantzigWolfeTurboDecoder(Decoder):
         b[0] = 1
         x = FTran(L, U, P, b)
         z = np.dot(c, x)
+        ki = np.zeros(self.m)
+        K = 0
         # init done
         for iteration in itertools.count():
-            if iteration > 10000:
-                break
+            for i in range(self.m):
+                if x[i] < 1e-8:
+                    ki[i] += 1
+                    x[i] = np.random.randint(2,256)
+                    if ki[i] > K:
+                        K = ki[i]
             print('iteration {}'.format(iteration))
             L, U, P = LU(B)
-            assert np.allclose(np.dot(L, U), np.dot(P, B))
             pi = BTran(L, U, P, c)
-            assert np.allclose(np.dot(B.T, pi), c)
             ans = self.pricing(L, U, P, pi)
             if ans is None:
                 self.objectiveValue = z
-                print(np.around(x, 2))
-                self.solution = np.around(np.dot(x, codewords), 8)
+                for i in np.flatnonzero(ki > 0):
+                    x[i] = 0
+                self.solution = np.around(np.dot(x, codewords), 5)
                 break
             Aj, cj_bar, cj, codeword = ans
             Ajbar = FTran(L, U, P, Aj)
-            assert np.allclose(np.dot(B, Ajbar), Aj)
             delta = np.inf
             delta_ind = -1
-            for j in range(self.m):
-                if Ajbar[j] > 1e-6:
-                    if x[j] / Ajbar[j] < delta:
-                        delta = x[j] / Ajbar[j]
-                        delta_ind = j
+            while delta == np.inf:
+                for j in np.flatnonzero(ki == K):
+                    if Ajbar[j] > 1e-8:
+                        if x[j] / Ajbar[j] < delta:
+                            delta = x[j] / Ajbar[j]
+                            delta_ind = j
+                if delta == np.inf:
+                    assert K > 0
+                    for i in np.flatnonzero(ki == K):
+                        x[i] = 0
+                        ki[i] -= 1
+                    K -= 1
             assert delta < np.inf
-            x -= delta*Ajbar
+            if delta < 1e-8:
+                print('D')
+            for i in range(self.m):
+                if ki[i] == K:
+                    x[i] -= delta*Ajbar[i]
+                    if np.abs(x[i]) < 1e-10:
+                        x[i] = 0
+            #x -= delta*Ajbar
             x[delta_ind] = delta
-            assert np.all(x >= 0)
-            z += delta*cj_bar
+            if not np.all(x >= 0):
+                print('x', x)
+                return
+            if K == 0:
+                z += delta*cj_bar
             c[delta_ind] = cj
             B[:, delta_ind] = Aj
             codewords[delta_ind] = codeword
-            dual = np.dot(pi, b)
-            print('gap={}'.format(np.abs(z - dual)))
-            assert np.allclose(x, np.dot(np.linalg.inv(B), b))
+            #assert np.allclose(x, np.dot(np.linalg.inv(B), b))
             #print('basis exchange in index {}'.format(delta_ind))
             
               
@@ -182,6 +198,7 @@ if __name__ == "__main__":
     from lpdecoding.codes.interleaver import Interleaver
     import random
     random.seed(34)
+    np.random.seed(2)
     interleaver = Interleaver.random(10)
     from lpdecoding.codes.convolutional import LTEEncoder
     from lpdecoding.codes.turbolike import StandardTurboCode
@@ -189,7 +206,7 @@ if __name__ == "__main__":
     from lpdecoding.decoders.trellisdecoders import CplexTurboLikeDecoder
     #code = StandardTurboCode(LTEEncoder(), interleaver, "testcode")
     code = LTETurboCode(40)
-    channel = AWGNC(coderate=code.rate, snr=1, seed=12981)
+    channel = AWGNC(coderate=code.rate, snr=-1, seed=12981)
     sig = SignalGenerator(code, channel, randomCodewords=True, wordSeed=247)
     llr = next(sig)
     print(llr)
@@ -197,5 +214,5 @@ if __name__ == "__main__":
     decoder.decode(llr)
     decoder2 = CplexTurboLikeDecoder(code, ip=False)
     decoder2.decode(llr)
-    print(decoder2.objectiveValue)
-    print(decoder2.solution)
+    print(decoder2.objectiveValue, decoder.objectiveValue)
+    print(decoder2.solution, decoder.solution)
